1484ef1509510c0fe57ec94b21e678a19755c54a
Adrian Stabiszewski <as@grundid.de>
2017-07-03 15:10:23 +0200

    speed test prepare

--- test/active.test.js
+++ test/active.test.js
@@ -1,38 +1,15 @@
 const test = require("tape");
-const turf = require("@turf/turf");
+const MeshIndex = require("../src/mesh_index");
 const fs = require("fs");
 
-test("geohub - overpass converter", {skip: false}, t => {
-  const overpass = JSON.parse(fs.readFileSync("./debug/overpass-data.json"));
-  const elementsById = {};
-
-  overpass.elements.forEach((element) => {
-    const key = `${element.type}-${element.id}`;
-    elementsById[key] = element;
-  });
-
-  const lineStrings = [];
-  overpass.elements.forEach((element) => {
-    if (element.type === "way") {
-      const line = [];
-      element.nodes.forEach((nodeId) => {
-        const key = `node-${nodeId}`;
-        const nodeElement = elementsById[key];
-        if (nodeElement) {
-          line.push([nodeElement.lon, nodeElement.lat]);
-        } else {
-          console.error("Node ", key, " missing");
-        }
-      });
-      if (line.length > 1) {
-        lineStrings.push(turf.lineString(line, element.tags));
-      }
-    }
-  });
-
-  fs.writeFileSync("./debug/overpass.geojson", JSON.stringify(turf.featureCollection(lineStrings), null, 1));
-  //console.log(JSON.stringify(turf.featureCollection(lineStrings), null, 1));
-
+test("geohub - route to endpoint simple-simple grid", {skip: false}, t => {
+  const fc = JSON.parse(fs.readFileSync("./debug/overpass.geojson"));
+  const features = fc.features;
+  console.time("Meshing");
+  const meshIndex = new MeshIndex(features);
+  console.timeEnd("Meshing");
+  t.equals(meshIndex.getMesh().length, 1997);
   t.end();
 });
+
 
