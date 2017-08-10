578485226fa6c80562f31d76e9823571b4cd7dee
Adrian Stabiszewski <as@grundid.de>
2017-08-10 11:10:50 +0200

    clenup

--- test/feature_utils.test.js
+++ test/feature_utils.test.js
@@ -64,9 +64,9 @@ test("geohub - Hasengasse bug", {skip: false}, t => {
   const rightLine = turf.lineString([[9.2202007, 49.1435182], [9.2202059, 49.1435552],
     [9.2202431, 49.143794], [9.2202326, 49.1438103], [9.2202022, 49.1438226], [9.2199531, 49.1439048],
     [9.2196177, 49.1440264], [9.2195749, 49.1440446], [9.2195515, 49.1440619]]);
-  const combinedCords = featureUtils.combineSameTypeFeatures([buttomLine, rightLine, leftLine]);
-  t.equals(combinedCords.length, 17);
-  t.equals(JSON.stringify(combinedCords), "[[9.2195515,49.1440619],[9.2195749,49.1440446],[9.2196177,49.1440264]," +
+  const combinedCoords = featureUtils.combineSameTypeFeatures([buttomLine, rightLine, leftLine]);
+  t.equals(combinedCoords.length, 17);
+  t.equals(JSON.stringify(combinedCoords), "[[9.2195515,49.1440619],[9.2195749,49.1440446],[9.2196177,49.1440264]," +
     "[9.2199531,49.1439048],[9.2202022,49.1438226],[9.2202326,49.1438103],[9.2202431,49.143794]," +
     "[9.2202059,49.1435552],[9.2202007,49.1435182],[9.2198119,49.1435483],[9.2194641,49.1435848]," +
     "[9.2194756,49.1436247],[9.2194933,49.1438193],[9.2195137,49.1439712],[9.2195249,49.1440132]," +
